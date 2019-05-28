852fa9c9e2be0cdfbe0df96d5ad709357e6ad778
jhanco07 <jhanco07@hotmail.com>
2019-05-27 22:09:13 -0500

    cambos casa

--- practica_simple/js/main.js
+++ practica_simple/js/main.js
@@ -5,12 +5,21 @@ new Vue({
         conectado: '',
         edad: 1,
         dias: ['Lunes', 'Martes', 'Miercoles', 'Jueves','Viernes' ],
-        newItem: []
+        newItem: '',
+        listItems:[]
     },
     methods:{
         changeValue(event){
             this.conectado=event.target.value;
             console.log(event.target.value);
+        },
+        addItemMethod(){
+            this.listItems.unshift(this.newItem);
+            this.newItem='';
+        },
+        alerta(){
+            alert('¡Ingresar datos!');
         }
+        
     }
 });
