640fbf357233d6905545479bb891b904ce535313
jhanco07 <jhanco07@@hotmail.com>
2019-06-05 13:43:13 -0500

    fltros

---
+++ practica_simple/js/filtros.js
@@ -0,0 +1,49 @@
+
+new Vue({
+    el: 'main',
+    data: {
+        mensaje: 'Hola Jhan Luis',
+        minimo: 5,
+        seleccionGenero: '',
+        juegosFiltrados:[],
+        juegosFiltrados2:[],
+        juegosPredecidos:[],
+        busquedaJuego: '',
+        textoPredict: '',
+        generos:[ {nombre: 'FPS'} , {nombre: 'Estrategia'}, {nombre: 'Deportes'},{nombre: 'Aventura'} ] ,
+        juegos:[
+            {titulo:'Battlefield 1', genero: 'FPS', puntuacion: 9},
+            {titulo:'Civilization VI', genero: 'Estrategia', puntuacion: 10},
+            {titulo:'FIFA 9', genero: 'Deportes', puntuacion: 7},
+            {titulo:'Nascar', genero: 'Deportes', puntuacion: 8},
+            {titulo:'ProEvolution Soccer', genero: 'Deportes', puntuacion: 4},
+            {titulo:'Nascar Rumble', genero: 'Deportes', puntuacion: 9},
+            {titulo:'Falling Skies', genero: 'Aventura', puntuacion: 6}
+    
+        ]
+    },
+    
+    methods:{
+        buscarJuegosXgenero(){
+           this.juegosFiltrados=[];
+           this.juegosFiltrados= this.juegos.filter((juego)=>juego.genero==this.seleccionGenero)
+        },
+        buscarJuego(){
+            this.juegosFiltrados2= this.juegos.filter((juego)=>juego.titulo.toLowerCase().includes(this.busquedaJuego.toLowerCase()))
+        },
+        predecirTexto(){
+           return this.juegosPredecidos= this.juegos.filter((juego)=>juego.titulo.toLowerCase().includes(this.textoPredict.toLowerCase()));
+        }
+    },
+    computed: {
+        mensajeAlReves(){
+            return  this.mensaje.split('').reverse().join('');
+        },
+        mejoresJuegos(){
+            return this.juegos.filter((juego)=> juego.puntuacion >=  this.minimo)
+        }
+        
+       
+    }
+
+});
--- practica_simple/js/main.js
+++ practica_simple/js/main.js
@@ -6,7 +6,17 @@ new Vue({
         edad: 1,
         dias: ['Lunes', 'Martes', 'Miercoles', 'Jueves','Viernes' ],
         newItem: '',
-        listItems:[]
+        listItems:[], 
+        mensaje: 'Hola Jhan Luis',
+        actividades: [
+            {nombre:'Actividad 1', activo: true},
+            {nombre:'Actividad 2', activo: true},
+            {nombre:'Actividad 3', activo: false},
+            {nombre:'Actividad 4', activo: true},
+            {nombre:'Actividad 5', activo: true},
+            {nombre:'Actividad 6', activo: true},
+            {nombre:'Actividad 7', activo: true}
+        ]
     },
     methods:{
         changeValue(event){
@@ -20,6 +30,16 @@ new Vue({
         alerta(){
             alert('¡Ingresar datos!');
         }
-        
+    },
+    computed: {
+        mensajeAlReves(){
+            return  this.mensaje.split('').reverse().join('');
+        },
+        listaFiltrada(){
+           return this.actividades.filter((tarea)=>{
+               return tarea.activo
+            });
+        }
     }
+
 });
